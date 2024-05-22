b53fab195c477f10d80f08e777390204b9509799
Alex Grishutin <agrishutin@neuro.net>
2024-05-22 13:19:04 +0300

    updated version and other package details

--- yandex_cloud_client/version.py
+++ yandex_cloud_client/version.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.5'
+__version__ = '1.1.0'
 __url__ = "https://github.com/akimrx/python-yc-client"
 __description__ = "PUnofficial Yandex.Cloud REST API Client"
-__author__ = "Akim Faskhutdinov"
+__author__ = "Akim Faskhutdinov, Alex Grishutin"
 __author_email__ = "akimstrong@yandex.ru"
+__status__ = "Development"
 __license__ = "GPLv3"
